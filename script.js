c82f6a30dd743e5b50837f791214d66cc5c5c309
sajjat <mdsajjathossen89@gmail.com>
2025-07-23 10:13:57 +0600

    v 0.1

--- script.js
+++ script.js
@@ -3,20 +3,40 @@ let plus= document.querySelector("#plus");
 let minus= document.querySelector("#minus");
 let reset= document.querySelector(".reset")
 let count=0;
+function updateDisplay(){
+    display.innerText= count;
+    if (count===10){
+        display.style.color= "red";
+    } else {
+        display.style.color ="black"
+    }
+
+    plus.disabled = count>=10;
+    minus.disabled = count<=0;
+}
 
 plus.addEventListener("click",function(){
-    count++;
-    display.innerText=count;
+    if(count<10){
+        count++;
+        updateDisplay();
+    if (count === 10){
+        alert("Reched max value");
+    }
+    } 
 });
 minus.addEventListener("click",function(){
     if(count>0){
         count--;
-        display.innerText=count;
-    }
+        updateDisplay();
+        if(count === 0){
+            alert("Reched min value");
+        }
+        }
 });
 
 reset.addEventListener("click",function(){
     count=0;
-    display.innerText=count;
+    updateDisplay();
 });
 
+window.onload = updateDisplay();
